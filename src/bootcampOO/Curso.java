86165d44b3bb224092030be6c37ec028db7b18f7
david.lima <fernanda.souza@eemovel.com>
2022-04-03 21:02:16 -0300

    commit test

---
+++ src/bootcampOO/Curso.java
@@ -0,0 +1,5 @@
+package bootcampOO;
+
+public class Curso {
+
+}
---
+++ src/bootcampOO/Main.java
@@ -0,0 +1,8 @@
+package bootcampOO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Projeto para o segundo desafio do bootcamp da DIO");
+	}
+}
